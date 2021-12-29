48422c58f3325c4267b167c76e54794f12e7b546
Andrew Kessler <andrew.kessler@10xgenomics.com>
2021-12-28 22:47:00 -0500

    Modified startup script to work with new strucutre

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,26 +5,31 @@
 # as a startup task.
 # (TODO: Move these to be managed by systemd instead)
 
+INSTALL_DIR=/home/pi/pisces-deploy
+
+export PYTHONPATH=$INSTALL_DIR/shared
+
+PY=python3
 #Kill if already running
 echo "Killing processes..."
 pkill -INT -f hardwareControl_server.py
 pkill -INT -f scheduler.py
 pkill -INT -f gui.py
 
-export DISPLAY=:0.0 
+export DISPLAY=:0.0
 
 sleep 1
 
 echo "Restarting server"
 #start the grpc server
-python /home/pi/Repositories/pisces/hal/hardwareControl_server.py &
+$PY $INSTALL_DIR/bin/hardwareControl_server.py &
 
 #launch the scheduler client task
 sleep 5
 echo "Restarting scheduler"
-python /home/pi/Repositories/pisces/hal/scheduler.py &
+$PY $INSTALL_DIR/bin/scheduler.py &
 
 sleep 1
 echo "Restarting GUI"
-python /home/pi/Repositories/pisces/hal/gui.py &
+$PY $INSTALL_DIR/bin/gui.py &
 
